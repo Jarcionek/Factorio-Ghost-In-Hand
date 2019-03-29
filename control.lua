3bff362c27204c3248d3b26c9a21c04c1904bdc5
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-03-29 20:47:12 +0000

    added multiplayer support

--- control.lua
+++ control.lua
@@ -1,25 +1,54 @@
-local held_item_name
-local ignore_next = false
+local function memory_of(player)
+    return global.player_memory[player.index]
+end
+
+local function init_player(player)
+    --if global.player_memory == nil then
+    --    return
+    --end
+
+    global.player_memory[player.index] = global.player_memory[player.index] or {}
+
+    memory_of(player).held_item_name = nil
+    memory_of(player).ignore_next = false
+end
+
+script.on_init(function()
+    global.player_memory = global.player_memory or {}
+    for _, player in pairs(game.players) do
+        init_player(player)
+    end
+end)
+
+script.on_event(defines.events.on_player_created, function(event)
+    init_player(game.players[event.player_index])
+end)
+
+script.on_event(defines.events.on_player_joined_game, function(event)
+    init_player(game.players[event.player_index])
+end)
+
+
 
 script.on_event(defines.events.on_player_cursor_stack_changed, function(event)
     local player = game.players[event.player_index]
 
-    if ignore_next then
-        ignore_next = false
+    if memory_of(player).ignore_next then
+        memory_of(player).ignore_next = false
         do
             return
         end
     end
 
     if player.cursor_stack ~= nil and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
-        held_item_name = player.cursor_stack.name
+        memory_of(player).held_item_name = player.cursor_stack.name
     else
-        player.cursor_ghost = held_item_name
+        player.cursor_ghost = memory_of(player).held_item_name
     end
 end)
 
 script.on_event("GhostInHand_clean-cursor", function(event)
     local player = game.players[event.player_index]
 
-    ignore_next = true
+    memory_of(player).ignore_next = true
 end)
