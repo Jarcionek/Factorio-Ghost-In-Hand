9d43de8f013dadbb8d6ac55c2c8fa1852c5f9f4b
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-03-29 21:25:22 +0000

    updated installation script to include the changelog

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ mkdir GhostInHand_1.0.0
 cp info.json GhostInHand_1.0.0
 cp README.md GhostInHand_1.0.0
 cp LICENSE GhostInHand_1.0.0
+cp changelog.txt
 cp thumbnail.png GhostInHand_1.0.0
 cp *.lua GhostInHand_1.0.0
 zip -r GhostInHand_1.0.0{.zip,}
