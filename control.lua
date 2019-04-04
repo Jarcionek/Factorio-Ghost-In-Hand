befc958fdea8c35c61b0c50814678a25febe3670
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-04-04 15:36:27 +0100

    wip 2

--- control.lua
+++ control.lua
@@ -49,11 +49,11 @@ end)
 -- the only event fired for building real tiles
 script.on_event(defines.events.on_player_built_tile, function(event)
     local player = game.players[event.player_index]
-    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
-        player.print("on_player_built_tile, count " .. player.cursor_stack.count)
-    else
-        player.print("on_player_built_tile, cursor invalid")
-    end
+--    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
+--        player.print("on_player_built_tile, count " .. player.cursor_stack.count)
+--    else
+--        player.print("on_player_built_tile, cursor invalid")
+--    end
 
     attempt_place_ghost_in_hands(player)
 end)
@@ -62,22 +62,22 @@ end)
 -- 1. placing item normally
 script.on_event(defines.events.on_built_entity, function(event)
     local player = game.players[event.player_index]
-    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
-        player.print("on_built_entity, count " .. player.cursor_stack.count)
-    else
-        player.print("on_built_entity, cursor invalid")
-    end
+--    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
+--        player.print("on_built_entity, count " .. player.cursor_stack.count)
+--    else
+--        player.print("on_built_entity, cursor invalid")
+--    end
 
     attempt_place_ghost_in_hands(player)
 end)
 
 script.on_event(defines.events.on_put_item, function(event)
     local player = game.players[event.player_index]
-    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
-        player.print("on_put_item, count " .. player.cursor_stack.count)
-    else
-        player.print("on_put_item, cursor invalid")
-    end
+--    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
+--        player.print("on_put_item, count " .. player.cursor_stack.count)
+--    else
+--        player.print("on_put_item, cursor invalid")
+--    end
 
     attempt_place_ghost_in_hands(player)
 end)
@@ -85,13 +85,24 @@ end)
 -- TODO what if players puts something in chest? while holding that item?
 script.on_event(defines.events.on_player_main_inventory_changed, function(event)
     local player = game.players[event.player_index]
-    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
-        player.print("on_player_main_inventory_changed, count " .. player.cursor_stack.count)
-    else
-        player.print("on_player_main_inventory_changed, cursor invalid")
-    end
+--    if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
+--        player.print("on_player_main_inventory_changed, count " .. player.cursor_stack.count)
+--    else
+--        player.print("on_player_main_inventory_changed, cursor invalid")
+--    end
 
     attempt_place_ghost_in_hands(player)
+
+    local last_held_item_name = memory_of(player).last_held_item_name
+
+    if memory_of(player).last_held_item_name
+            and player.cursor_ghost == last_held_item_name
+            and count_item(player, last_held_item_name) > 0 then
+        local item_stack = player.get_main_inventory().find_item_stack(last_held_item_name)
+        if player.cursor_stack.can_set_stack(item_stack) then --TODO or should I use swap stack here? is this going to duplicate items?
+            player.cursor_stack.set_stack(item_stack)
+        end
+    end
 end)
 
 -- scenarios to test:
