818a29c76d8619ee9147609820ab891112ddcc45
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-04-10 18:42:50 +0100

    another toolbelt related bugfix

--- control.lua
+++ control.lua
@@ -4,7 +4,6 @@ local function memory_of(player)
     if not global.player_memory[player.index] then
         global.player_memory[player.index] = {
             last_held_item_name = nil,
-            ignore_next = false,
             is_gui_open = false
         }
     end
@@ -15,11 +14,6 @@ end
 script.on_event(defines.events.on_player_cursor_stack_changed, function(event)
     local player = game.players[event.player_index]
 
-    if memory_of(player).ignore_next then
-        memory_of(player).ignore_next = false
-        return
-    end
-
     if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
         if player.cursor_stack.prototype.place_result or player.cursor_stack.prototype.place_as_tile_result then
             memory_of(player).last_held_item_name = player.cursor_stack.name
@@ -29,21 +23,20 @@ script.on_event(defines.events.on_player_cursor_stack_changed, function(event)
 
     else
         if player.cursor_ghost then
-            return
+            return -- player replaced held item with a ghost (by e.g. clicking on the toolbelt)
         end
         if player.mod_settings["GhostInHand_disable-when-gui-open"].value and memory_of(player).is_gui_open then
             return
         end
 
-        if memory_of(player).last_held_item_name then
-            player.cursor_ghost = memory_of(player).last_held_item_name
+        local last_held_item = memory_of(player).last_held_item_name;
+        if last_held_item then
+            if player.get_main_inventory().get_item_count(last_held_item) > 0 then
+                return -- player has that item in their inventory, so they must have put it down themselves (by using 'clean cursor' action or clicking the item on the toolbelt)
+            end
+            player.cursor_ghost = last_held_item
         end
     end
-end)
-
-script.on_event("GhostInHand_clean-cursor", function(event)
-    local player = game.players[event.player_index]
-    memory_of(player).ignore_next = true
 end)
 
 script.on_event(defines.events.on_gui_opened, function(event)
