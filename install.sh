5724cd4e457908b4d13018243d9fbd6b69f52b03
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2020-02-18 08:47:34 +0000

    bumped version to 1.0.5

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 # prepare zip file
-mkdir GhostInHand_1.0.4
-cp info.json GhostInHand_1.0.4
-cp README.md GhostInHand_1.0.4
-cp LICENSE GhostInHand_1.0.4
-cp thumbnail.png GhostInHand_1.0.4
-cp *.lua GhostInHand_1.0.4
-cp -R locale GhostInHand_1.0.4
-cp changelog.txt GhostInHand_1.0.4
-zip -r GhostInHand_1.0.4{.zip,}
+mkdir GhostInHand_1.0.5
+cp info.json GhostInHand_1.0.5
+cp README.md GhostInHand_1.0.5
+cp LICENSE GhostInHand_1.0.5
+cp thumbnail.png GhostInHand_1.0.5
+cp *.lua GhostInHand_1.0.5
+cp -R locale GhostInHand_1.0.5
+cp changelog.txt GhostInHand_1.0.5
+zip -r GhostInHand_1.0.5{.zip,}
 
 # move zip to factorio mods folder (overriding existing one if present)
-mv GhostInHand_1.0.4.zip "/cygdrive/c/Users/${USER}/AppData/Roaming/Factorio/mods"
+mv GhostInHand_1.0.5.zip "/cygdrive/c/Users/${USER}/AppData/Roaming/Factorio/mods"
 
 # cleanup
-rm -r GhostInHand_1.0.4
+rm -r GhostInHand_1.0.5
